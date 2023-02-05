bbb0cd3d03f9042d3f865638c18593eea480b182
Tri Wulandari <triwulandari3456@gmail.com>
2023-02-05 14:30:56 +0700

    edit

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-import http from "node:http";
+// import http from "node:http";
+import express from "express";
+import { writeFile } from "node:fs/promises";
+import { rename } from "node:fs/promises";
+import { mkdir } from "node:fs/promises";
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/a" && req.method === "GET") {
-    res.end("Hello world");
-  } else{
-    res.end("Not found.");
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/a" && req.method === "GET") {
+//     res.end("Hello world");
+//   } else {
+//     res.end("Not found.");
+//   }
+// });
+const app = express();
 
-  }
-  
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/api/file", async () => {
+  await mkdir("./data.txt");
 });
 
-server.listen(3000);
+app.get("/api/ubah", async () => {
+  await rename("./data.txt", "./data");
+});
+
+app.get("/api/tampil", async (req, res) => {
+  await writeFile("./data/data.txt", "Tri wulandari", "UTF8");
+  res.send("berhasil");
+});
+
+app.listen(3000);
 
 console.log("server berjalan di http://localhost:3000");
